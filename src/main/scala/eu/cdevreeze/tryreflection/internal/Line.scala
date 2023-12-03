9b3136147d920164f217480f6c53471861c5840d
Chris de Vreeze <chris.de.vreeze@caiway.net>
2023-12-03 19:25:06 +0100

    Added Line, to prevent most String concatenations

---
+++ src/main/scala/eu/cdevreeze/tryreflection/internal/Line.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024-2024 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.tryreflection.internal
+
+/**
+ * Support for generating a single line with indentation.
+ *
+ * @author
+ *   Chris de Vreeze
+ */
+final case class Line(indent: Int, line: String):
+
+  def makeString: String = s"${Line.space * indent}$line"
+
+  /**
+   * Adds numberOfSpaces to the indentation. This is a cheap operation involving no String concatenation.
+   */
+  def shiftRight(numberOfSpaces: Int): Line = withIndent(indent + numberOfSpaces)
+
+  def appendString(s: String): Line = Line(indent, line + s)
+
+  private def withIndent(newIndent: Int): Line = Line(newIndent, line)
+
+object Line:
+
+  private val space: String = ' '.toString
+
+  def apply(line: String): Line = Line(0, line)
+
+end Line
--- src/main/scala/eu/cdevreeze/tryreflection/internal/LineGroup.scala
+++ src/main/scala/eu/cdevreeze/tryreflection/internal/LineGroup.scala
@@ -22,19 +22,22 @@ package eu.cdevreeze.tryreflection.internal
  * @author
  *   Chris de Vreeze
  */
-final case class LineGroup(indent: Int, lines: Seq[String]):
+final case class LineGroup(indent: Int, lines: Seq[Line]):
 
-  def makeString: String = effectiveLines.mkString("\n")
+  def makeString: String = effectiveLines.map(_.makeString).mkString("\n")
 
+  /**
+   * Adds numberOfSpaces to the indentation. This is a cheap operation involving no String concatenation.
+   */
   def shiftRight(numberOfSpaces: Int): LineGroup = withIndent(indent + numberOfSpaces)
 
   def appendStringToLastLine(s: String): LineGroup =
     if lines.isEmpty then this
-    else LineGroup(indent, lines.updated(lines.size - 1, lines.last + s))
+    else LineGroup(indent, lines.updated(lines.size - 1, lines.last.appendString(s)))
 
   private def withIndent(newIndent: Int): LineGroup = LineGroup(newIndent, lines)
 
-  private def effectiveLines: Seq[String] = lines.map(line => s"${LineGroup.space * indent}$line")
+  private def effectiveLines: Seq[Line] = lines.map(_.shiftRight(indent))
 
 object LineGroup:
 
@@ -43,11 +46,13 @@ object LineGroup:
   val fourSpaces: String = space * 4
   val javaIndent = 4
 
-  def apply(lines: Seq[String]): LineGroup = LineGroup(0, lines)
+  def apply(lines: Seq[Line]): LineGroup = LineGroup(0, lines)
 
-  def of(lines: String*): LineGroup = apply(lines)
+  def of(lines: String*): LineGroup = apply(lines.map(Line.apply))
 
-  def from(lineGroups: LineGroup*): LineGroup =
-    LineGroup(lineGroups.flatMap(_.effectiveLines))
+  /**
+   * Creates a LineGroup from several LineGroup instances. This is a cheap operation involving no String concatenation.
+   */
+  def from(lineGroups: LineGroup*): LineGroup = LineGroup(lineGroups.flatMap(_.effectiveLines))
 
 end LineGroup
